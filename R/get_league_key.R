f94403f953d4061fab656319aa466ce41d89be15
Dan Morse <danmorse8642@gmail.com>
2022-11-13 00:18:50 -0800

    removed ampersand and added a null return in case of error

--- R/get_league_key.R
+++ R/get_league_key.R
@@ -1,9 +1,11 @@
 get_league_key <- function(league){
   if(tolower(league) == "ohl"){
-    key <- "2976319eb44abe94&="
+    key <- "2976319eb44abe94="
   } else if(tolower(league) == "whl"){
     key <- "41b145a848f4bd67"
   } else if(tolower(league) == "qmjhl" | tolower(league) == "lhjmq"){
     key <- "f322673b6bcae299"
+  } else {
+    key <- NULL
   }
 }
