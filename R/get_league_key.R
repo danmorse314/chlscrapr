045fe38c804444fe61b6f5529e044ce479417269
Dan Morse <danmorse8642@gmail.com>
2022-11-13 09:32:24 -0800

    had a superfluous equals sign for some reason

--- R/get_league_key.R
+++ R/get_league_key.R
@@ -1,6 +1,6 @@
 get_league_key <- function(league){
   if(tolower(league) == "ohl"){
-    key <- "2976319eb44abe94="
+    key <- "2976319eb44abe94"
   } else if(tolower(league) == "whl"){
     key <- "41b145a848f4bd67"
   } else if(tolower(league) == "qmjhl" | tolower(league) == "lhjmq"){
